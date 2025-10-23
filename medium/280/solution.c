90d78ee3e2137bce7c20f6e98415bdd1e0fbc7a4
SAJADnet <sajadsajad.sch@gmail.com>
2025-10-23 05:45:22 -0400

    add medium challenge 280

---
+++ medium/280/solution.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main(){
+
+	int a, b, c;
+
+	scanf("%d", &a);
+	scanf("%d", &b);
+	scanf("%d", &c);
+	
+	if (a*a == b*b + c*c || b*b == a*a + c*c || c*c == a*a + b*b)
+		printf("Yes");
+	else
+		printf("No");
+
+	return 0;
+}
