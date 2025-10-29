7f0694fb18b05dbcd0e26e0be0160826b0a4eafe
SAJADnet <sajadsajad.sch@gmail.com>
2025-10-29 12:50:05 -0400

    add easy challenge 591.

---
+++ easy/591/solution.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int main(){
+	int num;
+	char star[10] = "**********";
+
+	scanf("%d", &num);
+	
+
+	for (int i=0; i<num; i++){
+		for (int j=0; j<num; j++){
+			if ((i == 0) || (i == num-1))
+				printf("*");
+			else
+				if (j == 0 || j == num-1)
+					printf("*");
+				else
+					printf(" ");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
