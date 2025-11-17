29d4c7c273598923d4f1108ad4476d3ad1cc91b8
SAJADnet <sajadsajad.sch@gmail.com>
2025-11-17 13:04:06 -0500

    add medium challenge 3404.

---
+++ medium/3404/solution.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+int main(){
+	int weight;
+	float height;
+	float bmi;
+
+	scanf("%d", &weight);
+	scanf("%f", &height);
+
+	bmi = weight / (height*height);
+
+	printf("%.2f\n", bmi);
+
+	if (bmi < 18.5)
+		printf("Underweight");
+	else if (bmi >= 18.5 && bmi < 25)
+		printf("Normal");
+	else if (bmi >= 25 && bmi < 30)
+		printf("Overweight");
+	else if (bmi >= 30)
+		printf("Obese");
+
+	return 0;
+}
